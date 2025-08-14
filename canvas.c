75b844daa213834d647c849a21db4a72fcf384d2
Bob French <bobfrenc@buffalo.edu>
2025-08-14 09:49:03 -0400

    Added data transfer procedures

--- canvas.c
+++ canvas.c
@@ -1,6 +1,10 @@
 #include "canvas.h"
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #include <stdlib.h>
+#include <string.h>
 
 segment_t *
 new_segment(int16_t x, int16_t y, uint8_t color){
@@ -58,4 +62,96 @@ clear_canvas(segment_t **head, segment_t **tail){
 		free_segment(curr);
 	}
 	*head = *tail = NULL;
+}
+
+char
+send_ctrl(int sockfd, uint8_t type, uint8_t color, uint16_t num_points){
+	uint8_t ctrl[4];
+
+	ctrl[0] = type;
+	ctrl[1] = color;
+	num_points = htons(num_points);
+	memcpy(&ctrl[2], &num_points, sizeof(num_points));
+	if(send(sockfd, ctrl, sizeof(ctrl), 0) <= 0) return 0;
+	return 1;
+}
+
+char
+recv_ctrl(int sockfd, uint8_t *color, uint16_t *num_points){
+	uint8_t ctrl[4];
+
+	if(!recv(sockfd, ctrl, sizeof(ctrl), 0) <= 0) return 0;
+	*color = ctrl[1];
+	memcpy(num_points, &ctrl[2], sizeof(uint16_t));
+	*num_points = ntohs(*num_points);
+	return ctrl[0];
+	return 1;
+}
+
+char
+send_segment(int sockfd, segment_t *segment){
+	point_t *curr;
+	int16_t *buff;
+	uint16_t num_points, i;
+
+	num_points = 0;
+	for(curr = segment->head; curr; curr = curr->next) num_points++;
+
+	if(!send_ctrl(sockfd, SEGMENT, segment->color, num_points)) return 0;
+
+	i = 0;
+	buff = malloc(sizeof(int16_t) * num_points * 2);
+	for(curr = segment->head; curr; curr = curr->next){
+		buff[i++] = htons(curr->x);
+		buff[i++] = htons(curr->y);
+	}
+	if(send(sockfd, buff,  sizeof(int16_t) * num_points * 2, 0) <= 0){
+		free(buff);
+		return 0;
+	}
+	free(buff);
+	return 1;
+}
+
+segment_t *
+recv_segment(int sockfd, uint8_t color, uint16_t num_points){
+	int16_t *buff;
+	int i;
+	segment_t *new;
+
+	buff = malloc(sizeof(int16_t) * num_points * 2);
+	if(recv(sockfd, buff, sizeof(int16_t) * num_points * 2, 0) <= 0){
+		free(buff);
+		return NULL;
+	}
+	new = new_segment(ntohs(buff[0]), ntohs(buff[1]), color);
+	for(i = 2; i < num_points * 2; i += 2)
+		add_point(new, ntohs(buff[i]), ntohs(buff[i + 1]));
+	free(buff);
+	return new;
+}
+
+char
+start_point_stream(int sockfd, uint8_t color, int16_t x, int16_t y){
+	int16_t buff[2];
+
+	if(!send_ctrl(sockfd, POINT_STREAM_START, color, 0)) return 0;
+	buff[0] = htons(x);
+	buff[1] = htons(y);
+	if(send(sockfd, buff, sizeof(buff), 0) <= 0) return 0;
+	return 1;
+}
+
+char
+end_point_stream(int sockfd){
+	return send_ctrl(sockfd, POINT_STREAM_START, 0, 0);
+}
+
+char
+recv_point(int sockfd, segment_t *segment){
+	int16_t buff[2];
+
+	if(recv(sockfd, buff, sizeof(buff), 0) <= 0) return 0;
+	add_point(segment, ntohs(buff[0]), ntohs(buff[1]));
+	return 1;
 }
--- canvas.h
+++ canvas.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+enum ctrl_types{POINT_STREAM_START = 1, POINT_STREAM_END, SEGMENT};
+
 typedef struct point point_t;
 struct point{
 	int16_t x;
@@ -32,5 +34,26 @@ add_segment(segment_t *, segment_t **, segment_t **);
 
 void
 clear_canvas(segment_t **, segment_t **);
+
+char
+send_ctrl(int, uint8_t, uint8_t, uint16_t);
+
+char
+recv_ctrl(int, uint8_t *, uint16_t *);
+
+char
+send_segment(int, segment_t *);
+
+segment_t *
+recv_segment(int, uint8_t, uint16_t);
+
+char
+start_point_stream(int, uint8_t, int16_t, int16_t);
+
+char
+end_point_stream(int);
+
+char
+recv_point(int, segment_t *);
 
 #endif
